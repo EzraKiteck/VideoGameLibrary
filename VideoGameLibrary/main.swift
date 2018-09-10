7d95971e718735933a7b65ec46969bf0dd1122d6
Ezra Kiteck <ezrakiteck@Interapts-MacBook-Pro-46.local>
2018-09-10 12:46:49 -0500

    Created Project

---
+++ VideoGameLibrary/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VideoGameLibrary
+//
+//  Created by Ezra Kiteck on 9/10/18.
+//  Copyright © 2018 Ezra Kiteck. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
